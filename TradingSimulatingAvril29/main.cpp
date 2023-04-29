8ba189039f014a7c6fa60cb67a21dba42316daae
FadiFadhlaoui1212 <130895466+FadiFadhlaoui1212@users.noreply.github.com>
2023-04-29 14:02:03 +0100

    Add files via upload

---
+++ TradingSimulatingAvril29/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "PrixJournalier.h"
+#include "boursevecteur.h"
+#include "PersistancePrixJournaliers.h"
+#include "simulation.h"
+using namespace std;
+
+int main()
+{
+ PersistancePrixJournaliers extract ;
+    vector<PrixJournalier> historique ;
+
+
+    historique=extract.lirePrixJournaliersDUnFichier("prices_simple.csv");
+    date d1;
+    cout<<"donner la date de début"<<endl;
+    cin>>d1;
+    date d2;
+    cout<<"donner la date de fin"<<endl;
+    cin>>d2;
+    date d;
+    cout<<"donner la date courante"<<endl;
+    cin>>d;
+    double budget;
+    cout<<"donner le budget"<<endl;
+    cin>>budget;
+    Simulation S(d1,d2,d,budget);
+    Bourse* bv = new BourseVecteur(historique);
+    TraderAleatoire* Tr = new TraderAleatoire();
+    double x;
+    x=S.executer(*bv,*Tr,S);
+    cout<<x;
+
+    return 0 ;
+}
