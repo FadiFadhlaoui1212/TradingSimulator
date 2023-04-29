b12ebc3044607e376416da30c3ca4b667bb8ed84
FadiFadhlaoui1212 <130895466+FadiFadhlaoui1212@users.noreply.github.com>
2023-04-29 15:52:43 +0100

    Update main.cpp

--- TradingSimulatingAvril29/main.cpp
+++ TradingSimulatingAvril29/main.cpp
@@ -12,24 +12,47 @@ int main()
 
 
     historique=extract.lirePrixJournaliersDUnFichier("prices_simple.csv");
-    date d1;
-    cout<<"donner la date de d�but"<<endl;
-    cin>>d1;
-    date d2;
-    cout<<"donner la date de fin"<<endl;
-    cin>>d2;
-    date d;
-    cout<<"donner la date courante"<<endl;
-    cin>>d;
+    int jj1;
+    cout<<"donner le jour de la date de début";
+    cin>>jj1;
+    int mm1;
+    cout <<"donner le mois de la date de début";
+    cin>>mm1;
+    int aaaa1;
+    cout <<"donner l'année de la date de début";
+    cin>>aaaa1;
+    date d1(jj1,mm1,aaaa1);
+    int jj2;
+    cout<<"donner le jour de la date de fin";
+    cin>>jj2;
+    int mm2;
+    cout <<"donner le mois de la date de fin";
+    cin>>mm2;
+    int aaaa2;
+    cout <<"donner l'année de la date de fin";
+    cin>>aaaa2;
+    date d2(jj2,mm2,aaaa2);
+    int jj;
+    cout<<"donner le jour de la date courante";
+    cin>>jj;
+    int mm;
+    cout <<"donner le mois de la date courante";
+    cin>>mm;
+    int aaaa;
+    cout <<"donner l'année de la date courante";
+    cin>>aaaa;
+    date d(jj,mm,aaaa);
     double budget;
     cout<<"donner le budget"<<endl;
     cin>>budget;
     Simulation S(d1,d2,d,budget);
-    Bourse* bv = new BourseVecteur(historique);
+    BourseVecteur bv(historique);
     TraderAleatoire* Tr = new TraderAleatoire();
     double x;
-    x=S.executer(*bv,*Tr,S);
+    x=S.executer(bv,*Tr,S);
+    cout<<"le budget final est";
     cout<<x;
+
 
     return 0 ;
 }
